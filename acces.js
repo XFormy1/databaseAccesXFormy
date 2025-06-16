e6d3dc83c9c10ad47a0ad16b50c7a02aed004a96
XFormy1 <theraskaraqila@gmail.com>
2025-06-16 20:05:18 +0800

    first commit

---
+++ acces.js
@@ -0,0 +1,10 @@
+"creator": "6283899977000",
+  "secretCode": "XFormy",
+  "description": "gatau.",
+  "lastUpdated": "31-05-2025",
+  "author": "kaska",
+  "status": "Active",
+  "license": "MIT",
+  "version": "5.1.2",
+  "Access": [
+    "6283899977000"
